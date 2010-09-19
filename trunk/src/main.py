c996b459c3d58c355c66320c581597334d0cd1c8
sajal83@gmail.com <sajal83@gmail.com@148ff556-1abf-b7f7-bb65-3eb0b1b1214b>
2010-09-19 16:09:02 +0000

    Made ConsumeBeacon class

--- trunk/src/main.py
+++ trunk/src/main.py
@@ -4,13 +4,29 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
     
-    
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('Under Construction!')
 
 
-application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+class ConsumeBeacon(webapp.RequestHandler):
+
+    def consume(self, id, vars=[]):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Consuming.... --%s-- nom nom nom...' % (id))
+        self.response.out.write(vars)
+
+    def get(self, id):
+        vars = [{arg: self.request.get(arg)}  for arg in self.request.arguments()]
+        self.consume(id, vars )
+
+    def post(self, id):
+        self.consume(id)
+
+application = webapp.WSGIApplication([('/', MainPage),
+                                     ('/consume/(.*)', ConsumeBeacon)
+                                     ],
+                                      debug=True)
 
 
 def main():
